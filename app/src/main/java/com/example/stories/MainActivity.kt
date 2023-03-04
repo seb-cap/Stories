fafc81d5ced9d5411c3a5cc57b1da21d34b6d34b
Sebastian <sebcap@uw.edu>
2023-03-04 02:36:52 -0800

    Added all personal stories from class

--- app/src/main/java/com/example/stories/MainActivity.kt
+++ app/src/main/java/com/example/stories/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.stories
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    StoriesApp(modifier = Modifier.fillMaxSize())
+                    StoriesApp()
                 }
             }
         }
@@ -33,6 +31,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     StoriesTheme {
-        StoriesApp(modifier = Modifier.fillMaxSize())
+        StoriesApp()
     }
 }
--- app/src/main/java/com/example/stories/Stories.kt
+++ app/src/main/java/com/example/stories/Stories.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
@@ -22,16 +23,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+const val FINAL_SLIDE = 7
+
 @Composable
-fun StoriesApp(
-    modifier: Modifier = Modifier
-) {
-    var slide by remember {mutableStateOf(0)}
+fun StoriesApp() {
+    var slide by rememberSaveable {mutableStateOf(0)}
     Box {
         Image(
             painter = when (slide) {
                 0 -> painterResource(R.drawable.stella_young)
                 1 -> painterResource(R.drawable.julian_crop)
+                2 -> painterResource(R.drawable.warren_crop)
+                3 -> painterResource(R.drawable.kenassa_crop)
+                4 -> painterResource(R.drawable.aj_crop)
+                5 -> painterResource(R.drawable.anna_crop)
+                6 -> painterResource(R.drawable.deej_crop)
+                7 -> painterResource(R.drawable.owen_crop)
                 else -> painterResource(R.drawable.ic_launcher_background)
             },
             contentDescription = "Image of speaker",
@@ -45,9 +52,29 @@ fun StoriesApp(
             when (slide) {
                 0 -> StellaYoung()
                 1 -> Julian()
+                2 -> Warren()
+                3 -> Kenassa()
+                4 -> AJ()
+                5 -> Anna()
+                6 -> DEEJ()
+                7 -> Owen()
                 else -> Box {}
             }
-            Footer(slide, {slide = it + 1}, {slide = it - 1})
+            Footer(
+                page = slide,
+                increase = {
+                    slide = it + 1
+                    if (slide > FINAL_SLIDE) {
+                        slide = 0
+                    }
+                },
+                decrease = {
+                    slide = it - 1
+                    if (slide < 0) {
+                        slide = FINAL_SLIDE
+                    }
+                }
+            )
         }
     }
 }
@@ -55,7 +82,9 @@ fun StoriesApp(
 @Composable
 fun BasicSlide(title: String, subtitle: String, description: String, linkText: String, url: String, modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxWidth().fillMaxHeight(0.9F)
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.9F)
     ) {
         TitleAndSubtitle(title = title, sub = subtitle, modifier = modifier)
         DescriptionAndLink(
@@ -68,7 +97,7 @@ fun BasicSlide(title: String, subtitle: String, description: String, linkText: S
 }
 
 @Composable
-fun StellaYoung(modifier: Modifier = Modifier) {
+fun StellaYoung() {
     BasicSlide(
         title = "Stella Young",
         subtitle = "I am not your inspiration, thank you very much",
@@ -79,10 +108,10 @@ fun StellaYoung(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Julian(modifier: Modifier = Modifier) {
+fun Julian() {
     BasicSlide(
         title = "Julian's Story",
-        subtitle = "Pride in signing",
+        subtitle = "Pride in sign language",
         description = "Julian, an African American Caucasian boy who is deaf describes his experiences at the Washington School for the Deaf, and explains why people should learn to sign.",
         linkText = "Tap here to watch.",
         url = "https://www.youtube.com/watch?v=Iz3_n5g4LRs&ab_channel=OfficeoftheEducationOmbuds"
@@ -90,15 +119,89 @@ fun Julian(modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun Warren() {
+    BasicSlide(
+        title = "Warren's Story",
+        subtitle = "Tools for school with ADHD",
+        description = "Student with disability Warren Lybbert, a 9th grader at Quincy High School, describes in his own voice the challenges he faces and how assistive technology helps him.",
+        linkText = "Tap here to watch.",
+        url = "https://www.youtube.com/watch?v=PJ7jK_IyOPs&ab_channel=OfficeoftheEducationOmbuds"
+    )
+}
+
+@Composable
+fun Kenassa() {
+    BasicSlide(
+        title = "Kenassa's Story",
+        subtitle = "Expression with Autism",
+        description = "Kenassa, a Japanese, Chinese and Oromo (East African) student with disabilities talks about his experiences at school, and explains how Autism is a way of understanding from a different perspective.",
+        linkText = "Tap here to view.",
+        url = "https://www.youtube.com/watch?v=ZUzupiG1D3Y&ab_channel=OfficeoftheEducationOmbuds"
+    )
+}
+
+@Composable
+fun AJ() {
+    BasicSlide(
+        title = "AJ's Story",
+        subtitle = "Wheeling towards inclusion",
+        description = "Middle school student with disability Angelina (A.J.) Gammons-Reese, a 7th grade African American of Haitian decent at Meridian Parent Partnership Program,  describe in their own voice the challenges they face and their desire to be included.",
+        linkText = "Tap here to watch.",
+        url = "https://www.youtube.com/watch?v=1XQyuMHUwrc&ab_channel=OfficeoftheEducationOmbuds"
+    )
+}
+
+@Composable
+fun Anna() {
+    BasicSlide(
+        title = "Anna's Story",
+        subtitle = "Blindness is more of a social disability than a physical disability",
+        description = "Anna, a 12th grader at Issaquah High School shares her experiences as a student with disabilities and her hopes for the future.",
+        linkText = "Tap here to watch",
+        url = "https://www.youtube.com/watch?v=8GrgEOsfl_M&ab_channel=OfficeoftheEducationOmbuds"
+    )
+}
+
+@Composable
+fun DEEJ() {
+    BasicSlide(
+        title = "DJ Savarese",
+        subtitle = "Text-to-voice independence",
+        description = "Abandoned by his birth parents and presumed incompetent, DJ Savarese (“Deej”) found not only a loving family but also a life in words, which he types on a text-to-voice synthesizer. As he makes his way through high school and dreams of college, he confronts the terrors of his past, society's obstacles to inclusion, and the sometimes paralyzing beauty of his own senses. In his advocacy on behalf of other nonspeaking autistics, he embraces filmmaking and poetry, and discovers what having a voice can truly mean.",
+        linkText = "Tap here to learn more.",
+        url = "https://www.deejmovie.com/"
+    )
+}
+
+@Composable
+fun Owen() {
+    BasicSlide(
+        title = "Owen Suskind",
+        subtitle = "Expression via Disney with Autism",
+        description = "By evocatively interweaving classic Disney sequences with verité scenes from Owen’s life, the film explores how identification and empathy with characters like Simba, Jafar, and Ariel create a context for him to understand his feelings and interpret reality. Beautiful, original animations further give form to Owen’s rich dialogue with all the Disney classics as he imagines himself heroically facing adversity in a tribe of sidekicks.",
+        linkText = "Tap to watch the documentary",
+        url = "https://www.youtube.com/watch?v=oVMcYkRHtcU&ab_channel=YouTubeMovies"
+    )
+}
+
+@Composable
 fun TitleAndSubtitle(title: String, sub: String, modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxWidth().padding(top = 40.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 40.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(title, fontSize = 24.sp, color = Color.White)
+        Text(title, fontSize = 24.sp, color = Color.White, style = TextStyle(shadow = Shadow(
+            color = Color.Black,
+            blurRadius = 13F
+        )))
         Spacer(Modifier.size(16.dp))
-        Text(sub, color = Color.White)
+        Text(sub, color = Color.White, textAlign = TextAlign.Center, style = TextStyle(shadow = Shadow(
+            color = Color.Black,
+            blurRadius = 13F
+        )))
     }
 }
 
@@ -147,7 +250,9 @@ fun LinkText(modifier: Modifier = Modifier, str: String, textAlign: TextAlign = 
 
     val uriHandler = LocalUriHandler.current
     ClickableText(
-        modifier = modifier.padding(16.dp).fillMaxWidth(),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
         text = annotatedString,
         style = TextStyle(textAlign = textAlign),
         onClick = {
@@ -192,5 +297,5 @@ fun NavigationButton(text: String, page: Int, onClick: (Int) -> Unit) {
 @Preview(showBackground = true)
 @Composable
 fun StoriesPreview() {
-    StoriesApp(Modifier.fillMaxSize())
+    StoriesApp()
 }
